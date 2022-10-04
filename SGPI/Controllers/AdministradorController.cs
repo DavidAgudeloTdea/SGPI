5d73c918dec49f382ade524b3b857a30fbdb9f7b
ESTUDIANTE <ESTUDIANTE@DESKTOP-0R9U54R>
2022-10-04 10:48:56 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/AdministradorController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGPI.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult OlvidarContrasena()
+        {
+            return View();
+        }
+        public IActionResult CrearUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult BuscarUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult EliminarUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult ModificarUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+    }
+
+    public class CoordinadorController : Controller
+    {
+        public IActionResult BuscarCoordinador()
+        {
+            return View();
+        }
+
+        public IActionResult ProgramarAsignacion()
+        {
+            return View();
+        }
+
+        public IActionResult Homologacion()
+        {
+            return View();
+        }
+
+        public IActionResult Entrevistas()
+        {
+            return View();
+        }
+
+        public IActionResult CooReportes()
+        {
+            return View();
+        }
+    }
+
+    public class EstudianteController : Controller
+    {
+        public IActionResult Actualizar()
+        {
+            return View();
+        }
+
+        public IActionResult Pagos()
+        {
+            return View();
+        }
+    }
+}
